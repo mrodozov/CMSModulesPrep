232152ef2cacc872ba56fc2409fc30579f8786b8
Mircho Rodozov <mrodozov@cern.ch>
2018-05-15 17:31:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
 git clone https://github.com/Teemperor/ClangAutoModules.git
-git clone https://github.com/mrodozov/CMSModulesPrep.git
 cp CMSModulesPrep/* .
 wget https://teemperor.de/pub/module.log
 mv module.log src/module.modulemap
