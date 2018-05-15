e60a27d8562b93dcaf530994d67660e2be21b8c9
Mircho <mrodozov@cern.ch>
2018-05-15 17:29:30 +0200

    scripts and files adjusted for run under docker

--- clang_command.sh
+++ clang_command.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
 if [[ ! -d /tmp/$(whoami)/outputdir ]]; then
-mkdir /tmp/$(whoami)/outputdir
+mkdir -p /tmp/$(whoami)/outputdir
 fi
 
-#if [[ -e /tmp/$(whoami)/outputdir/log.log ]]; then
-#rm /tmp/$(whoami)outputdir/log.log
-#fi
+if [[ ! -e /tmp/$(whoami)/outputdir/log.log ]]; then
+touch /tmp/$(whoami)outputdir/log.log
+fi
 
 ./ClangAutoModules/ClangModules.py --modulemap-dir ${CMSSW_BASE}/ClangAutoModules/files --output-dir /tmp/$(whoami)/outputdir --vfs-output /tmp/$(whoami)/outputdir/vfs_folder  --log /tmp/$(whoami)/outputdir/log.log -I "/usr/include:/build/cmsbld/all/inst/slc7_amd64_gcc630/external/boost/1.63.0-omkpbe2/include" --invocation "clang -std=c++11"
