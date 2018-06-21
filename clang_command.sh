2dc812b14076eb73d9fd97c6e91a130da7c372be
Mircho Rodozov <mrodozov@cern.ch>
2018-06-21 15:40:46 +0200

    remove abs paths from clang script

--- clang_command.sh
+++ clang_command.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -x
 
-if [[ ! -d /tmp/$(whoami)/outputdir ]]; then
-mkdir -p /tmp/$(whoami)/outputdir
+export here=`pwd`
+
+if [[ ! -d $here/outputdir ]]; then
+mkdir -p $here/outputdir
 fi
 
-if [[ ! -e /tmp/$(whoami)/outputdir/log.log ]]; then
-touch /tmp/$(whoami)outputdir/log.log
+if [[ ! -e $here/outputdir/log.log ]]; then
+touch $here/outputdir/log.log
 fi
 
-./ClangAutoModules/ClangModules.py --modulemap-dir ${CMSSW_BASE}/ClangAutoModules/files --output-dir /tmp/$(whoami)/outputdir --vfs-output /tmp/$(whoami)/outputdir/vfs_folder  --log /tmp/$(whoami)/outputdir/log.log -I "/usr/include:/cvmfs/cms-ib.cern.ch/nweek-02528/slc7_amd64_gcc630/external/boost/1.63.0-omkpbe2/include" --invocation "clang -std=c++11"
+./ClangAutoModules/ClangModules.py \
+	--modulemap-dir ${CMSSW_BASE}/ClangAutoModules/files \
+	--output-dir $here/outputdir \
+	--vfs-output $here/outputdir/vfs_folder \
+	--log $here/outputdir/log.log \
+	-I "/usr/include:/cvmfs/cms-ib.cern.ch/nweek-02528/slc7_amd64_gcc630/external/boost/1.63.0-omkpbe2/include" --invocation "clang -std=c++11"
