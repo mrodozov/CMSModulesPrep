d6bb92753e57a34e4be56254c967127eccc4ac92
Mircho Rodozov <mrodozov@cern.ch>
2018-06-26 16:33:17 +0200

    add moduelscash error checking script

---
+++ utils/check_all_modules.sh
@@ -0,0 +1,1 @@
+for i in `ls modulescache`; do for j in `ls modulescache/$i`; do if [[ `cat modulescache/$i/$j/CMS_*.log | grep  error: | wc -l` > 0 ]]; then echo $i/$j; cat modulescache/$i/$j/CMS_*.log | grep  error: | wc -l; fi; done; done
