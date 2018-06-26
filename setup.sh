4a89782843188e3c373963115faf4436703594f3
Mircho Rodozov <mrodozov@cern.ch>
2018-06-26 16:49:09 +0200

    remove unused and reorder the folder

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-git clone https://github.com/Teemperor/ClangAutoModules.git
-cp CMSModulesPrep/* .
-#wget https://teemperor.de/pub/module.log
-cp module.modulemap src/module.modulemap
