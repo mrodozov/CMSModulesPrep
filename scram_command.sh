b0da18f9d62174b99918070be5a84beb474bdf7f
Mircho Rodozov <mrodozov@cern.ch>
2018-06-27 16:18:15 +0200

    update scram comm

--- scram_command.sh
+++ scram_command.sh
@@ -2,4 +2,6 @@
 
 #rm -rf $CMSSW_BASE/cppmodulescache/*
 
-USER_CXXFLAGS="-Wno-module-import-in-extern-c -fmodules-cache-path=/tmp/$(whoami)/cppmodulescache -fmodules -Xclang -fmodules-local-submodule-visibility -fcxx-modules -Wno-register -Rmodule-build -ivfsoverlay "/tmp/$(whoami)/outputdir/vfs_folder"" scram b -v -k -j $1 COMPILER=llvm 2>&1
+export here=`pwd`
+
+USER_CXXFLAGS="-Wno-module-import-in-extern-c -fmodules-cache-path=$here/cppmodulescache -fmodules -Xclang -fmodules-local-submodule-visibility -fcxx-modules -Wno-register -Rmodule-build -ivfsoverlay "$here/outputdir/vfs_folder"" scram b -v -k -j $1 COMPILER=llvm 2>&1
